e62a8c055f6e10cbd31a473d8b260691a8ed5fd0
Yuliana-Alcala <alcalabyulianac@gmail.com>
2024-11-13 16:17:39 +0100

    update ViewArmstrong

--- src/main/java/views/ViewArmstrong.java
+++ src/main/java/views/ViewArmstrong.java
@@ -6,27 +6,39 @@ import java.util.Scanner;
 
 public class ViewArmstrong {
     private Scanner scanner;
+
+    private static final String RESET = "\033[0m"; 
+    private static final String GREEN = "\033[32m"; 
+    private static final String RED = "\033[31m"; 
     
     public ViewArmstrong(){
         this.scanner = new Scanner(System.in);
     }
 
-    public int index(){         
-        System.out.println("Introduzca el número");        
+    public int index(){  
+              
+        System.out.println("|      Introduzca el número:          |");  
+        System.out.println(" -------------------------------------");       
         return scanner.nextInt();         
         
     }
 
     public void renderNumber(Integer number, Boolean result){
         
-        System.out.print("El número: "+ number + " ");       
+        System.out.println(" -------------------------------------");
+        System.out.print("|    El número: "+ number + " ");
+        
+       // System.out.print("El número: "+ number + " ");       
         if(result){
-            System.out.println("es Armstrong");
+            System.out.println(GREEN +"Es Armstrong      "+ RESET+"|");
+            
         }
         else
         {
-            System.out.println("no es Armstrong");
+            System.out.println(RED +"No es Armstrong   "+ RESET+"|");
+            
         }
+        System.out.println(" -------------------------------------");
     }
 
 }
